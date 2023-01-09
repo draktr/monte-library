87371c54714fb2f2df73b29dadbfb4c6a5a221d6
Dragan <dragan@u.yale-nus.edu.sg>
2023-01-09 17:16:04 +0100

    Added integrator tests

---
+++ tests/test_integrator.py
@@ -0,0 +1,27 @@
+import pytest
+from monte_carlo_integrator import integrator
+
+
+def test_one_dimensional():
+
+    def objective(args):
+        return args[0]**2
+
+    result = integrator(objective,
+                        lower_bounds = [-3],
+                        upper_bounds = [3],
+                        n = 10000000)
+
+    assert result - 18 <= 10**(-2)
+
+def test_two_dimensional():
+
+    def objective(args):
+        return args[0]**2 + args[1]**3
+
+    result = integrator(objective,
+                        lower_bounds = [-3, -3],
+                        upper_bounds = [3, 3],
+                        n = 10000000)
+
+    assert result - 108 <= 10**(-2)
