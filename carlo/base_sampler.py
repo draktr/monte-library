58bfa2d6cf5c763adb84979f5b23ef51e0107196
Dragan <dragan@u.yale-nus.edu.sg>
2023-01-10 23:53:30 +0100

    Added samples and acceptances as properties and added methods for calculating mean and standard deviation of samples, and sampling acceptance rate

--- carlo/base_sampler.py
+++ carlo/base_sampler.py
@@ -2,16 +2,30 @@
 Base sampler for Monte Carlo methods containing some basic utility methods.
 """
 
+import numpy as np
 import pandas as pd
 
 
 class BaseSampler():
 
     def __init__(self) -> None:
-        pass
+
+        self.samples = None
+        self.acceptances = None
 
     def save_samples(self,
-                     samples,
                      path = None):
 
-        pd.DataFrame(samples).to_csv(path)
+        pd.DataFrame(self.samples).to_csv(path)
+
+    def mean(self):
+
+        return np.mean(self.samples, axis=0)
+
+    def std(self):
+
+        return np.std(self.samples, axis=0)
+
+    def acceptance_rate(self):
+
+        return np.mean(self.acceptances)
