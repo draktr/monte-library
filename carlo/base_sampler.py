a16640c2c77a4e3bd9f9db802065304bf82b1337
Dragan <dragan@u.yale-nus.edu.sg>
2023-01-12 23:54:26 +0100

    Added histogram plotting method

--- carlo/base_sampler.py
+++ carlo/base_sampler.py
@@ -4,6 +4,7 @@ Base sampler for Monte Carlo methods containing some basic utility methods.
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 class BaseSampler():
@@ -29,3 +30,22 @@ class BaseSampler():
     def acceptance_rate(self):
 
         return np.mean(self.acceptances)
+
+    def plot_histogram(self,
+                       bins = 100,
+                       show = True,
+                       save = False):
+
+        dim = self.samples.shape[1]
+        fig = plt.figure(constrined_layout = True)
+        axs = [plt.subplot(dim, 1, i+1) for i in range (dim)]
+        for i in range(dim):
+            axs[i].hist(self.samples[:, i], bins = bins)
+            axs[i].set_xlabel(f"theta_{i}")
+            axs[i].set_ylabel("density")
+            axs[i].set_title(f"theta_{i} histogram")
+        fig.suptitle("Parameter Histograms")
+        if save is True:
+            plt.savefig("histograms.png", dpi=300)
+        if show is True:
+            plt.show()
