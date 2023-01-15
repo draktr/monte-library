c8863af6f2d553ffe847f35a420bc8f37ae2d635
Dragan <dragan@u.yale-nus.edu.sg>
2023-01-15 23:07:33 +0100

    Initial commit

---
+++ carlo/gibbs_sampler.py
@@ -0,0 +1,16 @@
+import numpy as np
+from base_sampler import BaseSampler
+
+
+class GibbsSampler(BaseSampler):
+
+    def __init__(self,
+                 target) -> None:
+
+        self.target = target
+
+    def _iterate(self):
+        pass
+
+    def sample():
+        pass
