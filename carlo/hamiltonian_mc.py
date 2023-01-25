fd0b96258944891f88734b2825c459d54f16b6f8
Dragan <dragan@u.yale-nus.edu.sg>
2023-01-25 18:43:22 +0100

    Added methods for Leapfrog integrator and alpha calculation

--- carlo/hamiltonian_mc.py
+++ carlo/hamiltonian_mc.py
@@ -11,19 +11,47 @@ class HamiltonianMC(BaseSampler):
         self.target_lp = target_lp
         self.target_lp_gradient = target_lp_gradient
 
+    def _hamiltonian(self,
+                     theta,
+                     rho):
+
+        return 0.5 * np.sum(rho ** 2) + -self.target_lp(theta)
+
+    def _acceptance(self,
+                    theta_proposed, rho_updated,
+                    theta_current, rho_current):
+
+        return min(0, -(self._hamiltonian(theta_proposed, rho_updated) - self._hamiltonian(theta_current, rho_current)))
+
+    def _leapfrog(self,
+                  theta,
+                  rho,
+                  epsilon,
+                  l,
+                  inverse_metric):
+
+        rho -= 0.5 * epsilon * -self.target_lp_gradient(theta)
+        for _ in range(l - 1):
+            theta += epsilon * inverse_metric * rho
+            rho -= 0.5 * epsilon * -self.target_lp_gradient(theta)
+        theta += epsilon * inverse_metric * rho
+        rho -= 0.5 * epsilon * -self.target_lp_gradient(theta)
+
+        return theta, rho
+
     def _iterate(self,
                  theta_current,
                  epsilon,
                  l,
                  metric,
-                 inverse_metric,):
+                 inverse_metric):
 
         rho_current = np.random.multivariate_normal(mean=np.zeros(theta_current.shape[0]),
                                                     cov=metric, size=theta_current.shape[0])
         theta_proposed, rho_updated = self._leapfrog(theta_current.copy(), rho_current.copy(),
                                                      epsilon, l, inverse_metric)
 
-        alpha = self._acceptance_step(theta_proposed, rho_updated, theta_current, rho_current)
+        alpha = self._acceptance(theta_proposed, rho_updated, theta_current, rho_current)
         u = np.random.rand()
         if u <= alpha:
             theta_new = theta_proposed
