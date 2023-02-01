9d7183fa3194ace4f6dfe68f2363d63807a612f3
Dragan <dragan@u.yale-nus.edu.sg>
2023-02-01 22:15:19 +0100

    Added test data, adjusted test method

--- tests/test_gibbs.py
+++ tests/test_gibbs.py
@@ -1,5 +1,14 @@
 import pytest
-from gibbs_sampler import GibbsSampler
+import numpy as np
+from scipy import stats
+from carlo.gibbs_sampler import GibbsSampler
+
+
+def generate_data(true_param, n):
+
+    x = np.linspace(-20, 20, n)
+    y = true_param[0] * x + true_param[1] + stats.norm(loc=0, scale=true_param[2])
+    return x, y
 
 
 def distributions():
@@ -13,6 +22,10 @@ def sampler(distributions):
 
 
 def test(sampler):
-    sampler.sample(iter=100000, warmup=1000, theta=0, lag=1)
 
-    assert sampler.mean() - 10 <= 10 ** (-2)
+    true_param = np.array([5, 10, 2])
+    x, y = generate_data(true_param=true_param, n=50)
+    sampler.sample(iter=100000, warmup=1000, theta=0, step_size=0.1, lag=1)
+
+    mean = sampler.mean()
+    assert mean - true_param <= np.repeat(10 ** (-2), len(mean))
--- tests/test_hamiltonian_mc.py
+++ tests/test_hamiltonian_mc.py
@@ -1,5 +1,14 @@
 import pytest
-from hamiltonian_mc import HamiltonianMC
+import numpy as np
+from scipy import stats
+from carlo.hamiltonian_mc import HamiltonianMC
+
+
+def generate_data(true_param, n):
+
+    x = np.linspace(-20, 20, n)
+    y = true_param[0] * x + true_param[1] + stats.norm(loc=0, scale=true_param[2])
+    return x, y
 
 
 def target_lp():
@@ -12,13 +21,16 @@ def target_lp_gradietn():
 
 @pytest.fixture
 def sampler(target_lp, target_lp_gradient):
+
     sampler = HamiltonianMC(target_lp, target_lp_gradient)
     return sampler
 
 
 def test(sampler):
-    sampler.sample(
-        iter=100000, warmup=1000, theta=0, epsilon=0.1, l=10, metric=None, lag=1
-    )
 
-    assert sampler.mean() - 10 <= 10 ** (-2)
+    true_param = np.array([5, 10, 2])
+    x, y = generate_data(true_param=true_param, n=50)
+    sampler.sample(iter=100000, warmup=1000, theta=0, step_size=0.1, lag=1)
+
+    mean = sampler.mean()
+    assert mean - true_param <= np.repeat(10 ** (-2), len(mean))
