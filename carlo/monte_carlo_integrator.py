317699eb968b31f2800a4bf90b510a6aa21808a1
Dragan <dragan@u.yale-nus.edu.sg>
2023-02-16 10:58:19 +0100

    Changed docs and bounds length specification for ValueError

--- carlo/monte_carlo_integrator.py
+++ carlo/monte_carlo_integrator.py
@@ -20,14 +20,15 @@ def integrator(integrand, lower_bounds, upper_bounds, n):
     :type upper_bounds: ndarray
     :param n: Number of simulation samples
     :type n: int
+    :raises ValueError: Error raised if bounds provided are of different dimensions
     :return: Approximate integral value
     :rtype: float
     """
 
-    if len(lower_bounds) != len(upper_bounds):
+    if lower_bounds.shape[0] != upper_bounds.shape[0]:
         raise ValueError("Lower and upper bounds are of different dimensions.")
 
-    x = np.random.uniform(lower_bounds, upper_bounds, (n, len(lower_bounds)))
+    x = np.random.uniform(lower_bounds, upper_bounds, (n, lower_bounds.shape[0]))
 
     summation = 0
     for i in x:
