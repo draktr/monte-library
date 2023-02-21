8fc2c6855b30a0c5380fc32fd07b5ea46104d262
Dragan <dragan@u.yale-nus.edu.sg>
2023-02-21 16:46:24 +0100

    Added checks specific to gibbs sampler and hamiltonian mc

--- carlo/checks.py
+++ carlo/checks.py
@@ -8,3 +8,29 @@ def _check_posterior(posterior):
         raise ValueError(
             f"Posterior should be a callable. Current posterior is:{type(posterior)}"
         )
+
+
+def _check_posterior_gradient(gradient):
+    if not callable(gradient):
+        raise ValueError(
+            f"Posterior gradient should be a callable. Current posterior gradient is:{type(gradient)}"
+        )
+
+
+def _check_sampling_distributions(distributions):
+    if not isinstance(distributions, np.ndarray):
+        distributions = np.asarray(distributions)
+    for distribution in distributions:
+        if not callable(distributions[distribution]):
+            raise ValueError(
+                f"Distribution {distribution} should be callable. {distribution} is {type(distribution)}"
+            )
+    return distributions
+
+
+def _check_dimensions(theta, distributions):
+    if theta.shape[0] != distributions.shape[0]:
+        raise ValueError(
+            f"There should be one and only one parameter per sampling distribution. \
+                Currently there are {theta.shape[0]} parameters and {distributions.shape[0]} distributions."
+        )
