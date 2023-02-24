f21071edae0e8387289f76007f7e71dc5f999f32
Dragan <dragan@u.yale-nus.edu.sg>
2023-02-24 18:23:03 +0100

    Changed basic description and author

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # carlo
-Carlo is a set of basic Monte Carlo methods in Python. The package is written to be both clear to understand and flexible.
+
+Carlo is a set of Monte Carlo methods in Python. The package is written to be flexible, clear to understand and encompass variety of Monte Carlo methods.
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
 """
 Carlo
 
-Carlo is a set of Monte Carlo methods in Python. The package is written to be both flexible and clear to understand.
+Carlo is a set of Monte Carlo methods in Python. The package is written to be flexible, clear to understand and encompass variety of Monte Carlo methods.
 """
 
 __version__ = "0.1.0"
-__author__ = "Dragan"
+__author__ = "github.com/draktr"
