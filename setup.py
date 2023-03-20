86dfc19bd72d34cebbf1621f0d44f6e2a855ff81
Dragan <dragan@u.yale-nus.edu.sg>
2023-03-20 09:30:39 +0100

    Added long_description and changed classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup
+import re
+
+with open("README.md", "r") as f:
+    desc = f.read()
+    desc = desc.split("<!-- content -->")[-1]
+    desc = re.sub("<[^<]+?>", "", desc)
 
 setup(
     name="monte",
     version="0.1.0",
     description="Monte is a set of Monte Carlo methods in Python. The package is written to be flexible, clear to understand and encompass variety of Monte Carlo methods.",
+    long_description=desc,
+    long_description_content_type="text/markdown",
     url="https://github.com/draktr/monte",
     author="draktr",
     license="MIT License",
     packages=["monte"],
+    python_requires=">=3.6",
     install_requires=[
         "numpy",
         "scipy",
@@ -16,16 +25,25 @@ setup(
         "seaborn",
         "statsmodels",
     ],
+    keywords="montecarlo monte carlo, optimization, integration, sampling, mcmc, hmc, simulation, modelling",
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Intended Audience :: Science/Research",
         "Intended Audience :: Education",
         "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: MacOS",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
     ],
+    project_urls={
+        "Documentation": "https://monte.readthedocs.io/en/latest/",
+        "Issues": "https://github.com/draktr/monte/issues",
+    },
 )
